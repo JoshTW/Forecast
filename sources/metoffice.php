63af820c7c637944e38f33fa46b210b708d7746b
Josh Trotter-Wanner <jjtrotter@gmail.com>
2012-10-30 05:04:05 -0700

    Added locations and Latitude and Longitude values

---
+++ sources/metoffice.php
@@ -0,0 +1,102 @@
+<?php
+
+/*
+ * Source:
+ * http://www.metoffice.gov.uk/public/data/PWSCache/PollenForecast/Latest
+ * 
+ * Levels:
+ * Low
+ * Moderate
+ * High
+ * Very high
+ * 
+ * Locations:
+ * Orkney and Shetland, UK
+ * Orkney and Shetland
+ * 59.775°N 1.803°W
+ * <geometry><location>
+ *   <lat>59.7750000</lat>
+ *   <lng>-1.8030000</lng></location>
+ * <location_type>APPROXIMATE</location_type><viewport><southwest><lat>59.0482578</lat><lng>-3.8519502</lng></southwest><northeast><lat>60.4862572</lat><lng>0.2459502</lng></northeast></viewport></geometry>
+ * ll=59.052209,-2.981415
+ * 396.62638008981736 92.40164827990861
+ * 
+ * Highlands and Eilean Siar
+ * ll=55.877622,-5.115509
+ * 348.64722417679116 165.08080996020612
+ * 
+ * Grampian
+ * ll=57.230016,-2.617493
+ * 404.39724302346184 169.58081633483926
+ * 
+ * Strathclyde
+ * ll=55.877622,-5.115509
+ * 347.89722382120465 226.8308003323857
+ * 
+ * Central Tayside and Fife
+ * ll=56.260897,-3.162689
+ * 391.8972295118509 210.08081865887493
+ * 
+ * SW Scotland, Lothian Borders
+ * ll=55.446153,-3.304138
+ * 388.89722808950876 243.58079035216656
+ * 
+ * N Ireland
+ * ll=54.64525,-6.24057
+ * 316.75137635329554 267.52663507250963
+ * 
+ * Wales, UK
+ * Wales
+ * 51°29′N 3°11′W
+ * <geometry><location>
+ *   <lat>52.1306607</lat>
+ *   <lng>-3.7837117</lng></location>
+ * <location_type>APPROXIMATE</location_type><viewport><southwest><lat>51.3749686</lat><lng>-5.4824697</lng></southwest><northeast><lat>53.4356935</lat><lng>-2.6497994</lng></northeast></viewport><bounds><southwest><lat>51.3749686</lat><lng>-5.6700973</lng></southwest><northeast><lat>53.4356935</lat><lng>-2.6497994</lng></northeast></bounds></geometry>
+ * ll=52.400743,-3.468933
+ * 385.6472341338242 362.33081066560317
+ * 
+ * North West England
+ * NW England
+ * ll=53.712965,-2.550201
+ * 406.64724409021744 312.58079510579887
+ * 
+ * North East England
+ * NE England
+ * ll=54.612641,-1.425476
+ * 431.64722560232633 276.33079199939016
+ * 
+ * Yorkshire and the Humber
+ * Yorks & Humber
+ * ll=53.653592,-0.829468
+ * 445.7722360917797 313.97246684967445
+ * 
+ * East Midlands
+ * E Midlands
+ * ll=52.707179,-0.336456
+ * 455.647236981063 350.33080819520774
+ * 
+ * West Midlands
+ * W Midlands
+ * ll=52.503684,-1.934967
+ * 420.7722318241152 357.9724970434013
+ * 
+ * East of England
+ * E of England
+ * ll=52.287483,0.848694
+ * 52.24°N 0.41°E
+ * 484.3972202710996 365.58082535776884
+ * 
+ * South West England
+ * SW England
+ * ll=50.977453,-3.666687
+ * 50.96°N 3.22°W
+ * 380.64722417806877 415.08079229923237
+ * 
+ * London & South East England
+ * London and SE England
+ * ll=51.37178,-0.458679
+ * 51°30′N 0°5′W
+ * 453.0666204476265 400.3308245152491
+ * 
+ */
+?>
