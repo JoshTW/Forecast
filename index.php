7f91fc044e97ca230facc92e059807b24fbf811e
Josh Trotter-Wanner <jjtrotter@gmail.com>
2012-10-29 19:33:28 -0700

    Plan laid out

---
+++ index.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+        <title></title>
+    </head>
+    <body>
+        <?php
+        // This file is to ask the user to confirm/correct there location setting
+        //   - Using there IP address to find the pollen predictor serial number 
+        //     that last connected from the same IP
+        // This file should also ask the user to choose which of the forecasts 
+        // that are avalible for there location they want to recieve and in which order
+        // eg. Location = Exeter
+        //     Forecast one = Pollen forecast
+        //     Forecast two = wind speed
+        //     Forecast three = UV index
+        ?>
+    </body>
+</html>
